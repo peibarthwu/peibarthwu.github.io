8b0da740b110eb1c6ff5912d7c718e4d91de853d
Pei Pei Pomona Email Git Lab <mpba2018@mymail.pomona.edu>
2020-10-05 22:58:26 -0700

    disco ball added

--- placeImages.js
+++ placeImages.js
@@ -1,5 +1,5 @@
 var pics = [
-  "https://64.media.tumblr.com/478d18168a81626229d62b00171223db/9556cf9d042c50d6-97/s1280x1920/af7d3d326c97b2ab020af68145c4b3f321489f8e.png"
+  "img/stargirl.png, img/doodle.png, img/becco.png"
 ];
 
 var links = [
@@ -28,8 +28,8 @@ var imageInterval = setInterval(function() {
 }, 500);
 
 function placeImage(index) {
-  height = Math.random()*window.innerHeight;
-  width = Math.random()*window.innerWidth;
+  height = 10*window.innerHeight;
+  width = 10*window.innerWidth;
   z = index;
 
   var imgHTML = '<img src="'+pics[index]+'" style="top: '+height+'px;'+'right: '+width+'px;'+'z-index: '+z+';">';
