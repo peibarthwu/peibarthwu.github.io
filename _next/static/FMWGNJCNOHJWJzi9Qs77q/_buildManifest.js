b6ad94e769d760a53d3ae1a4b988f443ff25e233
peibarthwu <peibarthwu@gmail.com>
2022-12-24 16:02:27 -0500

    blue

--- _next/static/FMWGNJCNOHJWJzi9Qs77q/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/313-b68a673366462d33.js","static/chunks/pages/index-d9b27bbc4befd2c6.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/SuHdsFgendGwX4MUDwGss/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/313-b68a673366462d33.js","static/chunks/pages/index-2641749652a82338.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
