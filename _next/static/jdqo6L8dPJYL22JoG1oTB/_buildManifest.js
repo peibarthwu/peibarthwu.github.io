44d19dc389f44714d40f1ba7bb6c16dda29e2f50
peibarthwu <peibarthwu@gmail.com>
2023-01-04 19:37:37 -0500

    blue

--- _next/static/jdqo6L8dPJYL22JoG1oTB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/313-b68a673366462d33.js","static/chunks/pages/index-e30c3a4f93a23111.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
